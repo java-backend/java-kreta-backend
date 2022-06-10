05edc520a6c83ac9f5a606f646e568fd4050dc04
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-06-10 11:44:42 +0200

    Kréta maven projekt létrejött

---
+++ src/main/java/org/example/kreta/KretaApplication.java
@@ -0,0 +1,13 @@
+package org.example.kreta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KretaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KretaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/kreta/KretaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kreta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KretaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
