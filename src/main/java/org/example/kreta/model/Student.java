df136a783cc2921621164532bc9a72b2f430dccc
gycsaba <gyuris.csaba@vasvari.org>
2022-09-01 22:31:00 +0200

    Model rétegben kevesebb kód. Lombok megoldás. Java 19 pomxml

--- src/main/java/org/example/kreta/model/Student.java
+++ src/main/java/org/example/kreta/model/Student.java
@@ -1,8 +1,20 @@
 package org.example.kreta.model;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+
+//https://www.baeldung.com/intro-to-project-lombok
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 
 // https://www.baeldung.com/jpa-entities
 @Entity
@@ -14,68 +26,10 @@ public class Student {
 
     @Column(name="name", length=30, nullable=false, unique=false)
     private String fullName;
-    @Column(name = "birthday",nullable = false,unique = false)
+    @Column(name = "birthday",nullable = true,unique = false)
     private Calendar birthday;
     @Column(name = "iswoman",nullable = false,unique = false)
     private boolean isWoman;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Student() {
-        this.id = Long.valueOf(-1);
-        this.fullName = "";
-        this.birthday = new GregorianCalendar();
-        this.isWoman = true;
-    }
-
-    public Student(long iD, String fullName, Calendar birthday, boolean isWoman) {
-        this.id = iD;
-        this.fullName = fullName;
-        this.birthday = birthday;
-        this.isWoman = isWoman;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public Calendar getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Calendar birthday) {
-        this.birthday = birthday;
-    }
-
-    public boolean isWoman() {
-        return isWoman;
-    }
-
-    public void setWoman(boolean woman) {
-        isWoman = woman;
-    }
-
-    public boolean getIsWooman() {
-        return isWoman;
-    }
-
-    public boolean getIsMan() {
-        return !isWoman;
-    }
 
     @Override
     public String toString() {
