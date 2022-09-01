396a0fc5ecb8d2c513ca27871a2fb0bf45103872
gylm9 <gylm99@gmail.com>
2022-09-01 21:37:04 +0200

    Subject elkezdve

--- src/main/java/org/example/kreta/model/Subject.java
+++ src/main/java/org/example/kreta/model/Subject.java
@@ -1,12 +1,21 @@
 package org.example.kreta.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table (name="subject")
 public class Subject {
-
+    @Id
     private long id;
     private String subjectName;
 
+    public Subject() {
+        this.id=-1;
+        this.subjectName="";
+    }
     public Subject(long id, String subjectName) {
         this.id = id;
         this.subjectName = subjectName;
