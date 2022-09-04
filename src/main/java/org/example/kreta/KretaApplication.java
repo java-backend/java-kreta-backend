84ab469352e57aeb358e6d46a71e7496195a98fe
gycsaba <gyuris.csaba@vasvari.org>
2022-09-04 15:21:40 +0200

    Swagger

--- src/main/java/org/example/kreta/KretaApplication.java
+++ src/main/java/org/example/kreta/KretaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
 @SpringBootApplication
@@ -20,7 +23,10 @@ public class KretaApplication {
 
 	// https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
 	// http://localhost:8888/v2/api-docs
+	// http://localhost:8888/swagger-ui/
 	@Configuration
+	@EnableSwagger2
+	@Import(SpringDataRestConfiguration.class)
 	public class SpringFoxConfig {
 		@Bean
 		public Docket api() {
