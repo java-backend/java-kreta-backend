9caf01e05e4c12c18b89bc6f402ac2032d110aa1
gycsaba <gyuris.csaba@vasvari.org>
2022-09-04 16:10:38 +0200

    showStudent

--- src/main/java/org/example/kreta/controller/StudentsController.java
+++ src/main/java/org/example/kreta/controller/StudentsController.java
@@ -3,41 +3,23 @@ package org.example.kreta.controller;
 import org.example.kreta.model.Student;
 import org.example.kreta.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class StudentsController {
 
     @Autowired
-    StudentsService studentsService;
+    StudentsService service;
 
-    @GetMapping(value="/student",name="List of all students")
-    private List<Student> getAllStudents() {
-        return studentsService.getAllStudents();
-    }
-
-    @GetMapping(value = "/student/{studentid}", name = "Get student by id")
-    private Student getStudent(@PathVariable("studentid") Long studentid) {
-        return studentsService.getStudentById(studentid);
-    }
-
-    @DeleteMapping(value="/student/{studentid}",name = "Delete student")
-    private void deleteStudent(@PathVariable("studentid") Long studentid) {
-        studentsService.delete(studentid);
-    }
-
-
-    @PostMapping(value = "/student",name="Save new student")
-    private Long saveStudent(@RequestBody Student student) {
-        studentsService.saveOrUpdate(student);
-        return student.getId();
-    }
-
-    @PutMapping(value = "/student",name="Update student")
-    private Student update(@RequestBody Student student) {
-        studentsService.saveOrUpdate(student);
-        return student;
+    @GetMapping("/students/index")
+    public String showStudentsList(Model model)
+    {
+        List<Student> students = service.getAllStudents();
+        model.addAttribute("students",students);
+        return "students";
     }
 }
