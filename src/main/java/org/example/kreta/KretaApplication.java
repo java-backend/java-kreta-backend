57a841ce3ee4857ee37bdace622d54366f3fe122
gycsaba <gyuris.csaba@vasvari.org>
2022-09-04 15:15:44 +0200

    Springfox json

--- src/main/java/org/example/kreta/KretaApplication.java
+++ src/main/java/org/example/kreta/KretaApplication.java
@@ -2,7 +2,15 @@ package org.example.kreta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@EnableWebMvc
 @SpringBootApplication
 public class KretaApplication {
 
@@ -10,4 +18,17 @@ public class KretaApplication {
 		SpringApplication.run(KretaApplication.class, args);
 	}
 
+	// https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+	// http://localhost:8888/v2/api-docs
+	@Configuration
+	public class SpringFoxConfig {
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
+		}
+	}
 }
