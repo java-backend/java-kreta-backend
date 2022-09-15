bf8359a9a26a0fc8e23e627383a10ef604ccbe63
gylm9 <gylm99@gmail.com>
2022-09-15 14:22:36 +0200

    Subject form elkezdve

--- src/main/java/org/example/kreta/controller/SubjectsController.java
+++ src/main/java/org/example/kreta/controller/SubjectsController.java
@@ -5,9 +5,13 @@ import org.example.kreta.service.SubjectsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,4 +29,36 @@ public class SubjectsController {
 
             return mav;
         }
+
+       @GetMapping("/edit/{id}")
+        public String showUpdateForm (@PathVariable("id") long id, Model model){
+            Subject subject = service.getSubjectById(id);
+           model.addAttribute("subject", subject);
+           return "update-subject";
+        }
+
+        @PostMapping("/update/{id}")
+        public String updateSubject(@PathVariable("id") long id, @Valid Subject subject, BindingResult result, Model model) {
+            if (result.hasErrors()){
+                subject.setId(id);
+                return "update-subject";
+            }
+            service.saveOrUpdate(subject);
+            return "redirect:/index/";
+        }
+     /*   @GetMapping("/delete/{id}")
+        public String deleteUser (@PathVariable("id") long id, Model model) {
+            Subject subject = service.getSubjectById(id);
+            ser;
+        }*/
+
+
+       /* @PostMapping ("/adduser")
+        public ModelAndView showSubjectList(){
+            List<Subject> subjects = service.getAllSubjects();
+            ModelAndView mav=new ModelAndView("subjects/index");
+        }*/
+
+
+
 }
