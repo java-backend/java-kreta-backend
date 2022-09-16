1c6d00bbdf5ea18f312d2635c03e771f7d519d88
gycsaba <gyuris.csaba@vasvari.org>
2022-09-16 17:20:44 +0200

    Pagint result

---
+++ src/main/java/org/example/kreta/model/paging/PagingResult.java
@@ -0,0 +1,8 @@
+package org.example.kreta.model.paging;
+
+public class PagingResult {
+    private int numberOfItem;
+    private int pageSize;
+    private int currentPage;
+    private int numberOfPage;
+}
--- src/main/java/org/example/kreta/service/StudentsService.java
+++ src/main/java/org/example/kreta/service/StudentsService.java
@@ -67,5 +67,9 @@ public class StudentsService {
         }
         else
             return new ArrayList<Student>();
+
+
+
+
     }
 }
