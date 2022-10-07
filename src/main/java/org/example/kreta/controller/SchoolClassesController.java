080ffb035a1d131c17a711542bca3355a1b8872c
gylm9 <gylm99@gmail.com>
2022-10-07 16:47:19 +0200

    chool class student kesz

--- src/main/java/org/example/kreta/controller/SchoolClassesController.java
+++ src/main/java/org/example/kreta/controller/SchoolClassesController.java
@@ -6,9 +6,14 @@ import org.example.kreta.service.SchoolClassesService;
 import org.example.kreta.service.SubjectsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -26,5 +31,50 @@ public class SchoolClassesController {
         mav.addObject("schoolClasses",schoolClasses);
 
         return mav;
+    }
+
+    @GetMapping("/schoolClass/edit/{id}")
+    public String showUpdateForm (@PathVariable("id") long id, Model model){
+        SchoolClass schoolClass = service.getSchoolClassByID(id);
+        model.addAttribute("schoolClass", schoolClass);
+        return "/schoolClasses/update-schoolClass";
+    }
+
+    @PostMapping("/schoolClass/update/{id}")
+    public String updateSchoolClass(@PathVariable("id") Long id, @Valid SchoolClass schoolClass, BindingResult result, Model model) {
+        if (result.hasErrors()){
+            schoolClass.setId(id);
+            return "/schoolClasses/update-schoolClass";
+        }
+        service.saveOrUpdate(schoolClass);
+        return "redirect:/schoolClasses/index/";
+    }
+
+    @GetMapping("/schoolClass/delete/{id}")
+    public String deleteUser (@PathVariable("id") long id, Model model) {
+        SchoolClass schoolClass = service.getSchoolClassByID(id);
+        service.delete(id);
+        return "redirect:/schoolClasses/index";
+
+
+    }
+    @GetMapping ("/schoolClass/signup")
+    public ModelAndView showSingUpForm(){
+        SchoolClass newSchoolClass=new SchoolClass();
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("/schoolClasses/add-schoolClass");
+        mav.addObject("schoolClass",newSchoolClass);
+
+        return mav;
+    }
+
+    @PostMapping("/schoolClass/add-schoolClass")
+    public String addNewSchoolClass(@Valid SchoolClass schoolClass, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "/schoolClass/add-schoolClass";
+        }
+
+        service.saveOrUpdate(schoolClass);
+        return "redirect:/schoolClasses/index";
     }
 }
