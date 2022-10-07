e387bf1304bec20c2f6d4af929b520331b0a2eed
gylm9 <gylm99@gmail.com>
2022-10-07 16:55:11 +0200

    Apró javítások

--- src/main/java/org/example/kreta/controller/WellcomeController.java
+++ src/main/java/org/example/kreta/controller/WellcomeController.java
@@ -2,7 +2,7 @@ package org.example.kreta.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class WelcameController {
+public class WellcomeController {
 
     // https://studygyaan.com/spring-boot/thymeleaf-and-bootstrap-5-template-engine-in-spring-boot
     @GetMapping("/")
